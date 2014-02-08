c6d6e59dd40aad1860e01b7a90f34ab071349f71
Mark Fickett <mark.fickett@gmail.com>
2014-02-08 00:35:59 -0500

    deal cards

--- game_master.py
+++ game_master.py
@@ -8,17 +8,34 @@ import printing
 MIN_PLAYERS = 3
 MAX_PLAYERS = 5
 
+_INITIAL_CARDS = 13
+
 
 class GameMaster(object):
 
-  def __init__(self, players):
-    self._players = players
+  def __init__(self, player_objs):
+    self._players = player_objs
 
   def Play(self):
     self._board = modernart_pb2.Board(
         deck=_MakeDeck())
+    for player in self._players:
+      self._board.player_holdings.add(
+          name=player.name,
+          money=100,
+          hand=_TakeCards(self._board.deck, _INITIAL_CARDS))
     logging.debug('Starting board: %s', self._board)
     return self._players[0]
+
+
+def _TakeCards(source_cards, num):
+  if num > len(source_cards):
+    raise ValueError('Cannot take more than all cards.')
+  cards = list(source_cards)
+  del source_cards[:]
+  taken_cards = cards[:num]
+  source_cards.extend(cards[num:])
+  return taken_cards
 
 
 # acc. http://boardgamegeek.com/thread/239213/card-distribution-count
