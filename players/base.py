acde15ced4f2ed0dbfa504e3fad811f6e9e0ba99
Mark Fickett <mark.fickett@gmail.com>
2014-02-09 20:01:53 -0500

    notify player of new cards

--- players/base.py
+++ players/base.py
@@ -1,9 +1,18 @@
+import logging
+
+
 class PlayerWrapper(object):
 
   def __init__(self, player_obj, size):
     self._wrapped = player_obj
     self.name = player_obj.name
     self.size = size
+
+  def AcceptCards(self, cards):
+    try:
+      self._wrapped.AcceptCards(card.Copy() for card in cards)
+    except:
+      logging.error('Player failed to receive cards.', exc_info=True)
 
 
 class Player(object):
@@ -12,7 +21,11 @@ class Player(object):
   def __init__(self):
     self._name = 'Naive %s' % Player._inst_count
     Player._inst_count += 1
+    self._cards = []
 
   @property
   def name(self):
     return self._name
+
+  def AcceptCards(self, cards):
+    self._cards += cards
