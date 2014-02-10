9536c1202ce60d1efecfd0a409ecffc20aa42185
Mark Fickett <mark.fickett@gmail.com>
2014-02-10 00:03:05 -0500

    printing for cards, auction names

--- printing.py
+++ printing.py
@@ -10,3 +10,16 @@ def ArtistName(artist_id):
       part.upper() + '.' if len(part) == 1 else part.capitalize()
       for part in name.split('_'))
   return ' '.join(name_parts)
+
+
+def AuctionName(auction_id):
+  name = modernart_pb2.AuctionType.Id.Name(auction_id)
+  return '-'.join(part.capitalize() for part in name.split('_'))
+
+
+def Cards(cards):
+  descriptions = []
+  for card in cards:
+    descriptions.append(
+        '%s %s' % (AuctionName(card.auction_type), ArtistName(card.artist)))
+  return ', '.join(descriptions)
