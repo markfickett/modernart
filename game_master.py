05c6d81e05ed3973cc9e9badef6a0074ae4daca3
Mark Fickett <mark.fickett@gmail.com>
2014-02-10 00:04:44 -0500

    start and end (but do not yet run) auctions, get/give cards/money to/from players

--- game_master.py
+++ game_master.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import random
 
@@ -27,8 +28,6 @@ class GameMaster(object):
         'Player order is: %s',
         ', '.join(p.name for p in self._players))
     self._next_seller_index = 0
-    self._next_buyer_index = 0
-
 
   def Play(self):
     """Runs a game, returning the winning player."""
@@ -38,12 +37,12 @@ class GameMaster(object):
       self._board.player_holdings.add(
           name=player.name,
           money=_INITIAL_MONEY)
+      player.AcceptMoney(_INITIAL_MONEY)
 
     for round_index in range(_NUM_ROUNDS):
       self._PlayRound(round_index)
 
     return self._PickWinner()
-
 
   def _PlayRound(self, round_index):
     """Runs one round of the game: deals cards, runs auctions, pays."""
@@ -55,56 +54,202 @@ class GameMaster(object):
       self._players_by_name[holding.name].AcceptCards(new_cards)
     while True:
       self._StartAuction()
-      per_artist_counts = _CountPurchasesPerArtist(self._board.player_holdings)
-      if any(count > _NUM_TO_END_ROUND
-             for count in per_artist_counts.values()):
+      if self._board.auction.ends_round:
         break
       self._RunAuction()
-    self._RecordPlacings(per_artist_counts)
+      self._CompleteAuction()
+    self._RecordPlacings()
     self._AssignWinningsAndClearPurchases()
 
-
   def _StartAuction(self):
-    raise NotImplementedError()
+    """Gets the next card for auction from a Player.
 
+    Queries the next seller Player for a painting/card (or two, if one is a
+    double) for auction. If it's an open double, also gets the second card and
+    seller.
+
+    Updates the state of the auction submessage in the board, possibly ending
+    the round or assigning a de facto winner.
+    """
+    logging.info('Starting a new auction.')
+    self._board.ClearField('auction')
+
+    # Get a card from the next seller and validate it.
+    seller = self._players[self._next_seller_index]
+    self._next_seller_index = (self._next_seller_index + 1) % len(self._players)
+    cards = seller.GetCardsForAuction(self._board.auction)
+    logging.info(
+        '%s puts %s up for auction.', seller.name, printing.Cards(cards))
+    if not cards:
+      raise _FoulPlayException(seller, 'provided no cards for auction')
+    if len(cards) > 2:
+      raise _FoulPlayException(seller, 'provided > 2 cards for auction')
+    seller_holdings = self._GetHoldings(seller)
+    if not all(card in seller_holdings.hand for card in cards):
+      raise _FoulPlayException(
+          seller,
+          'put up %s for auction without having them (hand was %s)'
+          % (printing.Cards(cards), printing.Cards(seller_holdings.hand)))
+    is_double = any(card.auction_type == modernart_pb2.AuctionType.DOUBLE
+                    for card in cards)
+    if len(cards) == 2 and not is_double:
+      raise _FoulPlayException(
+          seller, 'provided two cards for auction, but no double')
+
+    # Do the bookkeeping to accept the card from the seller.
+    self._board.auction.seller_names.append(seller.name)
+    _MoveCards(cards, seller_holdings.hand, self._board.auction.cards)
+
+    self._CheckAndMaybeSetAuctionEndsRound()
+    if not self._board.auction.ends_round and is_double and len(cards) == 1:
+      self._AddSecondSeller()
+
+  def _CheckAndMaybeSetAuctionEndsRound(self):
+    """If current purchases + cards for auction are enough, ends the round."""
+    per_artist_counts = _CountPurchasesPerArtist(self._board.player_holdings)
+    for card in self._board.auction.cards:
+      per_artist_counts[card.artist] += 1
+    for artist, count in per_artist_counts.iteritems():
+      if count >= _NUM_TO_END_ROUND:
+        self._board.auction.ends_round = True
+        logging.info(
+            'Round ends with %d from %s.', count, printing.ArtistName(artist))
+
+  def _GetHoldings(self, player):
+    for holding in self._board.player_holdings:
+      if player.name == holding.name:
+        return holding
+    raise ValueError(
+        'player %s (name %s) not in recorded holdings'
+        % (player, player.name))
+
+  def _AddSecondSeller(self):
+    """In the case of an unclaimed double, finds the second seller."""
+    while True:
+      potential_seller = self._players[self._next_seller_index]
+      self._next_seller_index = (
+          self._next_seller_index + 1) % len(self._players)
+      if potential_seller.name == self._board.auction.seller_names[0]:
+        logging.info(
+            '%s gets the %s for free.',
+            potential_seller.name,
+            printing.Cards(self._board.auction.cards))
+        self._board.auction.winner_name = potential_seller.name
+        return
+
+      cards = potential_seller.GetCardsForAuction(self._board.auction)
+      if not cards:
+        logging.info(
+            '%s passes on the %s.',
+            potential_seller.name,
+            printing.Cards(self._board.auction.cards))
+        continue
+
+      if len(cards) != 1:
+        raise _FoulPlayException(
+            potential_seller, 'provided > 1 card on a double')
+      card = cards[0]
+      logging.info(
+          '%s adds a card: %s',
+          potential_seller.name,
+          printing.Cards([card]))
+      if card.auction_type == modernart_pb2.AuctionType.DOUBLE:
+        raise _FoulPlayException(
+            potential_seller, 'played a double on a double')
+      if card.artist != self._board.auction.cards[0]:
+        raise _FoulPlayException(
+            potential_seller,
+            'played a %s on a %s (different artist)'
+            % (printing.Cards([card]),
+               printing.Cards([self._board.auction.cards[0]])))
+
+      holdings = self._GetHoldings(potential_seller)
+      _MoveCards([card], holdings.hand, self._board.auction.cards)
+      self._board.auction.seller_names.append(potential_seller.name)
+      self._CheckAndMaybeSetAuctionEndsRound()
+      return
 
   def _RunAuction(self):
-    raise NotImplementedError()
+    # FIXME
+    next_buyer_index = self._next_seller_index
+    buyer = self._players[next_buyer_index]
+    self._board.auction.winner_name = buyer.name
+    self._board.auction.winning_bid = 1
+    logging.info('Bogus auction! %s bids 1 and wins instantly.', buyer.name)
 
+  def _CompleteAuction(self):
+    winner = self._players_by_name[self._board.auction.winner_name]
+    winner_holdings = self._GetHoldings(winner)
+    bid = self._board.auction.winning_bid
+    logging.info(
+        '%s pays %d for %s.',
+        winner.name, bid, printing.Cards(self._board.auction.cards))
+    if bid > winner_holdings.money:
+      raise _FoulPlayException(
+          winner,
+          'bid %d and won but only has %d' % bid, winner_holdings.money)
+    winner_holdings.money -= bid
+    winner_holdings.purchases.extend(self._board.auction.cards)
+    winner.AcceptCards(self._board.auction.cards, from_auction=True)
 
-  def _RecordPlacings(self, per_artist_counts):
+    num_sellers = len(self._board.auction.seller_names)
+    if num_sellers == 1:
+      payouts = [bid]
+    elif num_sellers == 2:
+      # Split between the two, favoring the first (who played the double).
+      # Note that some instructions say second seller takes all.
+      payouts = [(bid + 1) / 2, bid / 2]
+    else:
+      raise RuntimeError('should be 1 or 2 sellers, but had %d', num_sellers)
+    for seller_name, payout in zip(self._board.auction.seller_names, payouts):
+      seller = self._players_by_name[seller_name]
+      self._GetHoldings(seller).money += payout
+      seller.AcceptMoney(payout)
+
+  def _RecordPlacings(self):
     """Records which artists' paintings placed (in order) this round."""
-    rankings = sorted(n, artist for artist, n in per_artist_counts.iteritems())
+    per_artist_counts = _CountPurchasesPerArtist(self._board.player_holdings)
+    rankings = sorted(
+        [(n, artist) for artist, n in per_artist_counts.iteritems()])
     outcome = self._board.round_outcomes.add()
     for (_, artist), value in zip(rankings, _ARTIST_VALUES):
+      logging.info('%s garners %d.', printing.ArtistName(artist), value)
       outcome.artist_outcomes.add(artist=artist, value=value)
-
 
   def _AssignWinningsAndClearPurchases(self):
     """Calculates cumulative painting values, pays players for purchases."""
     values = {}
     outcomes = self._board.round_outcomes
-    for outcome in outcomes[-1]:
+    for outcome in outcomes[-1].artist_outcomes:
       values[outcome.artist] = outcome.value
     for round_outcome in outcomes[:-1]:
-      for outcome in round_outcome:
+      for outcome in round_outcome.artist_outcomes:
         if outcome.artist in values:
           values[outcome.artist] += outcome.value
+    for artist, value in values.iteritems():
+      logging.info(
+          '%s is worth %d this round.', printing.ArtistName(artist), value)
 
     for holdings in self._board.player_holdings:
       winnings = 0
       for card in holdings.purchases:
         winnings += values.get(card.artist, 0)
+      player = self._players_by_name[holdings.name]
+      logging.info(
+          '%s gets paid %d for: %s',
+          player.name, winnings, printing.Cards(holdings.purchases))
       del holdings.purchases[:]
       holdings.money += winnings
-
+      player.AcceptMoney(winnings)
 
   def _PickWinner(self):
     """Returns the player with the most money."""
     winner_info = None
-    for holding in self._board.player_holdings:
-      if winner_info is None or holding.money > winner_info.money:
-        winner_info = holding
+    for holdings in self._board.player_holdings:
+      logging.info('%s finishes with %d', holdings.name, holdings.money)
+      if winner_info is None or holdings.money > winner_info.money:
+        winner_info = holdings
+    logging.info('%s is the winner!', winner_info.name)
     return self._players_by_name[winner_info.name]
 
 
@@ -127,6 +272,16 @@ def _TakeCards(source_cards, num):
   taken_cards = cards[:num]
   source_cards.extend(cards[num:])
   return taken_cards
+
+
+def _MoveCards(cards, src, dst):
+  for card in cards:
+    try:
+      src.remove(card)
+    except ValueError:
+      logging.error('%s not in %s', printing.Cards([card]), printing.Cards(src))
+      raise
+  dst.extend(cards)
 
 
 # Definition of how many of what cards there are in the deck.
@@ -204,3 +359,10 @@ def _CountPurchasesPerArtist(holdings):
     for card in holding.purchases:
       counts[card.artist] += 1
   return counts
+
+
+class _FoulPlayException(RuntimeError):
+
+  def __init__(self, player, reason):
+    super(_FoulPlayException, self).__init__(
+        '%s %s: foul play ends the game!' % (player.name, reason))
--- players/base.py
+++ players/base.py
@@ -1,5 +1,7 @@
 import logging
 
+import modernart_pb2
+
 
 class PlayerWrapper(object):
   """Insulates the game from the (untrustworhy / 3rd party) Players."""
@@ -9,11 +11,41 @@ class PlayerWrapper(object):
     self.name = player_obj.name
     self.size = size
 
-  def AcceptCards(self, cards):
+  def AcceptCards(self, cards, from_auction=False):
     try:
-      self._wrapped.AcceptCards(card.Copy() for card in cards)
+      self._wrapped.AcceptCards(
+          self._CopyCards(cards), from_auction=from_auction)
     except:
       logging.error('Player failed to receive cards.', exc_info=True)
+
+  def AcceptMoney(self, money):
+    try:
+      self._wrapped.AcceptMoney(money)
+    except:
+      logging.error('Player failed to receive money.', exc_info=True)
+
+  def GetCardsForAuction(self, auction):
+    try:
+      return self._CopyCards(
+          self._wrapped.GetCardsForAuction(self._CopyAuction(auction)))
+    except:
+      logging.error('Player did not provide cards for auction.', exc_info=True)
+      return []
+
+  @staticmethod
+  def _CopyCards(cards):
+    copies = []
+    for card in cards:
+      copy = modernart_pb2.Card()
+      copy.CopyFrom(card)
+      copies.append(card)
+    return copies
+
+  @staticmethod
+  def _CopyAuction(auction):
+    copy = modernart_pb2.Auction()
+    copy.CopyFrom(auction)
+    return copy
 
 
 class Player(object):
@@ -23,11 +55,20 @@ class Player(object):
   def __init__(self):
     self._name = 'Naive %s' % Player._inst_count
     Player._inst_count += 1
-    self._cards = []
+    self._cards_in_hand = []
+    self._money = 0
 
   @property
   def name(self):
     return self._name
 
-  def AcceptCards(self, cards):
-    self._cards += cards
+  def AcceptCards(self, cards, from_auction=False):
+    if not from_auction:
+      self._cards_in_hand += cards
+
+  def AcceptMoney(self, money):
+    self._money += money
+
+  def GetCardsForAuction(self, auction):
+    # Playing on a double is hard: skip it! Otherwise just play any card.
+    return [] if auction.cards else [self._cards_in_hand.pop()]
