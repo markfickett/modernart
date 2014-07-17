69b3d69dc27d7eb8f60e2cd29f395066ebe83d2e
Mark Fickett <mark.fickett@gmail.com>
2014-07-17 00:43:50 -0400

    reduce logging level to INFO

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import players
 
 logging.basicConfig(
     format='%(levelname)s %(asctime)s %(filename)s:%(lineno)s: %(message)s',
-    level=logging.DEBUG)
+    level=logging.INFO)
 
 
 if __name__ == '__main__':
