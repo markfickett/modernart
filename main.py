c5905f74fc8cc656e061a97e523342c4c40278e5
Mark Fickett <mark.fickett@gmail.com>
2015-02-04 19:29:34 -0500

    Add --batch option to main, to run multiple games and log aggregate stats.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import collections
 import logging
 
 import game_master
@@ -22,13 +23,30 @@ if __name__ == '__main__':
   parser.add_argument(
       '--interactive', '-i', action='count',
       help='Include an interactive player. Repeate for multiple interactives.')
+  parser.add_argument(
+      '--batch', '-b', type=int, default=1,
+      help='Run multiple games. Logging is reduced and aggregate stats shown.')
   args = parser.parse_args()
+  if args.interactive and args.batch > 1:
+    parser.error('No interactives allowed in batch runs.')
   try:
     players.VerifyPlayerCounts(args.num_players, args.interactive)
   except ValueError, e:
     parser.error(e.message)
 
-  player_objs = players.LoadPlayers(
-      num_players=args.num_players, num_interactive=args.interactive)
-  gm = game_master.GameMaster(player_objs)
-  gm.Play()
+  win_counts = collections.defaultdict(lambda: 0)
+  for game_num in xrange(args.batch):
+    player_objs = players.LoadPlayers(
+        num_players=args.num_players, num_interactive=args.interactive)
+    gm = game_master.GameMaster(player_objs)
+    if args.batch > 1:
+      gm.log.setLevel(logging.WARNING)
+    winner = gm.Play()
+    win_counts[winner.GetPlayerClassName()] += 1
+    if args.batch > 1:
+      logging.info(
+          '%4d: %s' % (
+              game_num,
+              ' '.join(
+                  '%8s: %3d %3d%%' % (name, wins, 100.0 * wins/(game_num + 1.0))
+                  for name, wins in sorted(win_counts.items()))))
